a17e74cb4b87f6ba5422c7d7bb44d9b5ac2ac6e9
Surya <surya7ms@gmail.com>
2025-09-13 21:50:24 -0500

    1. Added unit tests

--- ErrorSummarizer.Api/Services/LlmOptions.cs
+++
@@ -1,9 +0,0 @@
-namespace ErrorSummarizer.Api.Services;
-
-public sealed class LlmOptions
-{
-    public bool Enabled { get; set; } = false;
-    public string? Endpoint { get; set; }
-    public string? ApiKey { get; set; }
-    public int TimeoutSeconds { get; set; } = 8;
-}
